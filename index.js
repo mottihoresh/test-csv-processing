302ab1309ecf03ad1c4d442cb8796f701a81caaa
Motti Horesh <mottihoresh@gmail.com>
2015-01-18 13:19:42 -0500

    bench mark

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ console.log("starting");
 
 // Load CSV file
 var loadScv = function () {
+
+    var readingArray = [];
     console.log(new Date());
     var reader = csv.createCsvFileReader('data/sensor-readings.csv', {
         'seperator': ',',
@@ -17,13 +19,14 @@ var loadScv = function () {
     reader.setColumnNames(['date', 'reading']);
 
     reader.addListener('data', function(data) {
-        console.log(data);
+        readingArray.push(data);
         });
 
     reader.addListener('end', function(data) {
       //this gets called when it's finished loading the entire file     return allEntries;
       //
         console.log(new Date());
+        console.log(readingArray);
     });
 };
 
